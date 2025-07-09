636b3a00f35d0e5177672f7978d53a65c05b93c4
MatsRahmat <matsrahmathidayat@gmail.com>
2025-07-09 08:58:58 +0700

    fix: fixing import in index assets

--- src/assets/index.jsx
+++ src/assets/index.jsx
@@ -8,7 +8,7 @@ import faq from "./icons/Help.svg";
 import loan from "./icons/File.svg";
 import pipeline from "./icons/Layers.svg";
 import product from "./icons/product.svg";
-import speed_meter from "./icons/performance.svg";
+import speed_meter from "./icons/Performance.svg";
 import tools from "./icons/tools.svg";
 import whatsapp from "./icons/WhatsApp.svg";
 import notification from "./icons/Notification.svg";
